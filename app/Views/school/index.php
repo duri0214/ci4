dacedd9eeb7af310a79b05d9c4f1d4af0df335ea
yoshi <yoshitakaOkada0214@gmail.com>
2022-05-01 17:41:55 +0900

    レイアウト調整
    Signed-off-by: yoshi <yoshitakaOkada0214@gmail.com>

--- app/Views/school/index.php
+++ app/Views/school/index.php
@@ -25,87 +25,92 @@
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     </head>
     <body>
-        <h1>school</h1>
-        <h2>todo</h2>
-        <ul>
-            <li>カリキュラム、HRを選択したらajaxで以下の情報を表示する
-                <ul>
-                    <li>カリキュラムの学期リスト</li>
-                    <li>カリキュラムの現在の学期</li>
-                    <li>HRの出席情報</li>
-                </ul>
-            </li>
-        </ul>
-        <p>学校名: <?= $school->name ?? null ?></p>
-        <p>所在地: 〒<?= $school->zipcode ?? null ?> <?= $school->address ?? null ?></p>
-        <p>tel: <?= $school->tel ?? null ?></p>
-        <p>学校のカテゴリー: <?= $schoolCategory->name ?? null ?></p>
-
-        <label>
-            <select class="select2-curriculum form-control" name="curriculum">
-                <option></option>
-                <?php
-                if (!empty($curriculumChoices)) {
-                    foreach ($curriculumChoices as $curriculumChoice) {
-                        echo('<option>');
-                        echo($curriculumChoice->name.' '.$curriculumChoice->remark);
-                        echo('</option>');
+        <div class="container">
+            <h1>school</h1>
+            <h2>todo</h2>
+            <ul>
+                <li>カリキュラム、HRを選択したらajaxで以下の情報を表示する
+                    <ul>
+                        <li>カリキュラムの学期リスト</li>
+                        <li>カリキュラムの現在の学期</li>
+                        <li>HRの出席情報</li>
+                    </ul>
+                </li>
+            </ul>
+            <p>学校名: <?= $school->name ?? null ?></p>
+            <p>所在地: 〒<?= $school->zipcode ?? null ?> <?= $school->address ?? null ?></p>
+            <p>tel: <?= $school->tel ?? null ?></p>
+            <p>学校のカテゴリー: <?= $schoolCategory->name ?? null ?></p>
+    
+            <label>
+                <select class="select2-curriculum form-control" name="curriculum">
+                    <option></option>
+                    <?php
+                    if (!empty($curriculumChoices)) {
+                        foreach ($curriculumChoices as $curriculumChoice) {
+                            echo('<option>');
+                            echo($curriculumChoice->name.' '.$curriculumChoice->remark);
+                            echo('</option>');
+                        }
                     }
-                }
-                ?>
-            </select>
-        </label>
-
-        <label>
-            <select class="select2-homeroom form-control" name="curriculum">
-                <option></option>
-                <?php
-                if (!empty($homeroomChoices)) {
-                    foreach ($homeroomChoices as $homeroom) {
-                        echo('<option>');
-                        echo($homeroom->name.' '.$homeroom->remark);
-                        echo('</option>');
+                    ?>
+                </select>
+            </label>
+    
+            <label>
+                <select class="select2-homeroom form-control" name="curriculum">
+                    <option></option>
+                    <?php
+                    if (!empty($homeroomChoices)) {
+                        foreach ($homeroomChoices as $homeroom) {
+                            echo('<option>');
+                            echo($homeroom->name.' '.$homeroom->remark);
+                            echo('</option>');
+                        }
                     }
-                }
-                ?>
-            </select>
-        </label>
-
-
-        <p>カリキュラム（id=1）がもつすべての学期リスト:</p>
-        <?php
-        if (!empty($periods)) {
-            foreach ($periods as $period) : ?>
-                <li><?= $period->name ?> <?= $period->from_date->format('Y-m-d') ?> ～ <?= $period->to_date->format('Y-m-d') ?></li>
-            <?php endforeach;
-        } else {
-            echo '<p>レコードなし</p>';
-        }
-        ?>
-
-        <p>現在の学期:</p>
-        <?php if (!empty($currentPeriod)) { ?>
-            <li><?= $currentPeriod->name ?> <?= $currentPeriod->from_date->format('Y-m-d') ?> ～ <?= $currentPeriod->to_date->format('Y-m-d') ?></li>
-        <?php } else {
-            echo '<p>レコードなし</p>';
-        } ?>
-        
-        <p>出席情報:</p>
-        <?php
-        if (!empty($attends)) {
-            foreach ($attends as $attend) : ?>
-                <li><?= $attend->attend_date->format('Y-m-d') ?> <?= $attend->remark ?></li>
-            <?php endforeach;
-        } else {
-            echo '<p>レコードなし</p>';
-        }
-        ?>
-        
-        <p><a href="<?= route_to('lesson_list') ?>">授業管理へ</a></p>
-        <p><a href="<?= route_to('cert_list') ?>">資格管理へ</a></p>
-        <a href="<?= route_to('home') ?>">ポートフォリオへもどる</a>
-        
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                    ?>
+                </select>
+            </label>
+    
+    
+            <p>カリキュラム（id=1）がもつすべての学期リスト:</p>
+            <?php
+            if (!empty($periods)) {
+                foreach ($periods as $period) : ?>
+                    <li><?= $period->name ?> <?= $period->from_date->format('Y-m-d') ?> ～ <?= $period->to_date->format('Y-m-d') ?></li>
+                <?php endforeach;
+            } else {
+                echo '<p>レコードなし</p>';
+            }
+            ?>
+    
+            <p>現在の学期:</p>
+            <?php if (!empty($currentPeriod)) { ?>
+                <li><?= $currentPeriod->name ?> <?= $currentPeriod->from_date->format('Y-m-d') ?> ～ <?= $currentPeriod->to_date->format('Y-m-d') ?></li>
+            <?php } else {
+                echo '<p>レコードなし</p>';
+            } ?>
+            
+            <p>出席情報:</p>
+            <?php
+            if (!empty($attends)) {
+                foreach ($attends as $attend) : ?>
+                    <li><?= $attend->attend_date->format('Y-m-d') ?> <?= $attend->remark ?></li>
+                <?php endforeach;
+            } else {
+                echo '<p>レコードなし</p>';
+            }
+            ?>
+            
+            <p><a href="<?= route_to('lesson_list') ?>">授業管理へ</a></p>
+            <p><a href="<?= route_to('cert_list') ?>">資格管理へ</a></p>
+            <a href="<?= route_to('home') ?>">ポートフォリオへもどる</a>
+        </div>
+        <script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                crossorigin="anonymous">
+        </script>
     </body>
 </html>
 
