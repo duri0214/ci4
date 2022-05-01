62e837284ac89c6a1330bd878eadf25ad58e53d6
yoshi <yoshitakaOkada0214@gmail.com>
2022-05-01 10:38:56 +0900

    不要コードの削除
    Signed-off-by: yoshi <yoshitakaOkada0214@gmail.com>

--- public/assets/school/js/addIncDec.js
+++ public/assets/school/js/addIncDec.js
@@ -1,4 +1,4 @@
-// TODO: テキストエリアを増減する外部jsファイルを作る
+// テキストエリアを増減する外部jsファイルを作る
 // https://4ndan.com/blog/form/
 // https://qiita.com/Yuji-207/items/0b0bd72330974186be8c
 
@@ -17,18 +17,3 @@ function addDecreaseAbility(obj)
 {
 
 }
-
-// button
-const btn_clone = $('.btn-clone');  // 追加ボタン
-const btn_remove = $('.btn-remove');  // 削除ボタン
-
-// clone
-btn_clone.click();
-
-// remove
-btn_remove.click(function () {
-    $('.text').last().remove();
-    if ($('.text').length < 2) {
-        btn_remove.hide();  // inputが2つ未満のときに削除ボタンを非表示
-    }
-});
