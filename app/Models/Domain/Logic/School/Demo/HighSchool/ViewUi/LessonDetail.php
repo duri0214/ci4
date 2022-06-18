5cae8f45fa29adc43770f0c5569508283a4904b1
yoshi <yoshitakaOkada0214@gmail.com>
2022-06-19 00:08:31 +0900

    component系のクラスを実装
    Signed-off-by: yoshi <yoshitakaOkada0214@gmail.com>

---
+++ app/Models/Domain/Logic/School/Demo/HighSchool/ViewUi/LessonDetail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\Domain\Logic\School\Demo\HighSchool\ViewUi;
+
+use App\Models\Domain\Component\AbstractView;
+use App\Models\Domain\Component\Button\Button;
+use App\Models\Domain\Component\Style;
+use App\Models\Domain\Logic\Collection;
+
+class LessonDetail extends AbstractView
+{
+    public function __construct()
+    {
+        $style = new Style();
+        $style->setBackgroundColor('#FF4500');
+        
+        $this->reportButtons = new Collection();
+        $this->reportButtons->addItem(new Button('成績一覧表1', 'https://www.google.com/'));
+        $this->reportButtons->addItem(new Button('成績一覧表2', 'https://www.google.com/'));
+        $this->reportButtons->addItem(new Button('成績一覧表3（成績会議用）', 'https://www.google.com/', $style));
+        $this->reportButtons->addItem(new Button('成績一覧表4', 'https://www.google.com/'));
+    }
+    
+    /**
+     * @return string
+     */
+    public function reportButtonsAsHtml(): string
+    {
+        $html = '';
+        if (count($this->reportButtons->getItems()) > 0) {
+            foreach ($this->reportButtons->getItems() as $item) {
+                $style = $item->getStyleAsHtml();
+                $onClick = $item->getOnClickAsHtml();
+                $caption = $item->getCaption();
+                $html .= "<button style='$style' type='button' onclick='$onClick'>$caption</button>";
+            }
+        }
+    
+        return $html;
+    }
+}
