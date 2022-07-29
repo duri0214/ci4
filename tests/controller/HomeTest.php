179f9d51d08bd1498a6597ceaa9158c7b030258e
yoshi <yoshitakaOkada0214@gmail.com>
2022-07-29 23:08:10 +0900

    ページ遷移の自動テストを実装
    Signed-off-by: yoshi <yoshitakaOkada0214@gmail.com>

--- tests/controller/HomeTest.php
+++ tests/controller/HomeTest.php
@@ -20,10 +20,31 @@ class HomeTest extends CIUnitTestCase
     public function testIndex()
     {
         // Get a simple page
-        $result = $this->call('get', '/');
-        $result->assertOK();
-        
-        $result = $this->call('get', '/home/store');
-        $result->assertOK();
+        $path_list = [
+            '/',
+            '/home/store',
+            // '/home/csv_export', // TODO: CSVとかPDFはまだできてない
+            // '/home/excel_export',
+            // '/home/rotate_pdf',
+            '/school',
+            '/school/admin/user/list',
+            '/school/admin/lesson/list',
+            '/school/cert/list',
+            '/school/report/menu',
+            '/school/admin/menu',
+            '/school/admin/user/1/detail',
+            '/school/admin/user/create',
+            '/school/admin/user/list',
+            '/school/admin/lesson/1/evaluation/list',
+            '/school/admin/lesson/create',
+            '/school/upload/lesson',
+            '/school/cert/1/item/list',
+            '/school/report/enrollment',    // 在籍証明書のPDFは通るな...
+            '/school/admin/unregistered/list',
+        ];
+        foreach ($path_list as $item) {
+            $result = $this->call('get', $item);
+            $result->assertOK();
+        }
     }
 }
